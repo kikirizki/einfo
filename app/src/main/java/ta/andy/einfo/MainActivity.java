3d657ae72ce2569ed7d9939da27d0bb80b222ad4
kiki <kiki@note.com>
2021-03-23 14:37:32 +0700

    Add internet permision

--- app/src/main/java/ta/andy/einfo/MainActivity.java
+++ app/src/main/java/ta/andy/einfo/MainActivity.java
@@ -15,9 +15,10 @@ public class MainActivity extends Activity
 		// constructor requires that you provide a channel ID. This is required for compatibility with Android 8.0 (API level 26) and higher, but is ignored by older versions.
 		NotificationCompat.Builder builder = new NotificationCompat.Builder(this )
 			.setSmallIcon(R.drawable.ic_launcher)
-			.setContentTitle("Memek ")
+			.setContentTitle("Title ")
+			.setCategory(NotificationCompat.CATEGORY_SOCIAL)
 			.setContentText("Konten Informasi")
-			.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+			.setPriority(NotificationCompat.PRIORITY_HIGH);
 			
 		NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         // notificationId is a unique int for each notification that you must define
