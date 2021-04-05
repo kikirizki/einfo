252007ee8d6f059e7d44dd472b46335bc413e913
Kiki Rizki <kkrzkrk@gmail.com>
2021-04-05 15:39:40 +0700

    Create Jadwal Activity and xml

--- app/src/main/java/ta/andy/einfo/JadwalActivity.java
+++ app/src/main/java/ta/andy/einfo/JadwalActivity.java
@@ -5,18 +5,14 @@ import android.content.*;
 import android.os.*;
 import android.view.*;
 
-public class ProfileActivity extends Activity 
+public class JadwalActivity extends Activity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profil);
+        setContentView(R.layout.jadwal);
 
     }
-	public void go2jadwal(View view){
-		Intent intent = new Intent(this,JadwalActivity.class);
-	    startActivity(intent);
-	}
-	
+
 }
